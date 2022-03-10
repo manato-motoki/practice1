2f10cddcb296ada4d31f5a036dc2798e4ba85fac
Manato Motoki <m.motoki.sys21@morijyobi.ac.jp>
2022-03-10 10:22:52 +0900

    pra2.javaのプログラムの修正
    Hello Worldの出力を1回から100回に変更しました。

--- pra2.java
+++ pra2.java
@@ -1,1 +1,10 @@
-System.out.println("Hello World");
+public class main{
+	public static void main(String[] args){
+	
+		int i;
+		for(i = 0, i < 100, i++){
+		System.out.println("Hello World");
+		}
+	}
+}
+
